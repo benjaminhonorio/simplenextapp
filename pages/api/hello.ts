5430b2e9140e10680f9b18fe0cdd67db4fb24158
Benjamin Honorio <benjaminhonorio.12@gmail.com>
2022-07-18 22:37:56 -0500

    Add simple input search + api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  age: number;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { name } = req.query;
+
+  const names = { benja: 28, alex: 22, leo: 17 };
+
+  if (name && typeof name !== "object") {
+    const found = names[name as keyof typeof names];
+    return res.status(200).json({ age: found });
+  }
+
+  res.status(200).json({ age: 0 });
 }
