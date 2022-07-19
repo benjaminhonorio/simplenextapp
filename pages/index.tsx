b70bf9138c83c435c9a7f5a41b3cdbeaeee3a6ab
Benjamin Honorio <benjaminhonorio.12@gmail.com>
2022-07-19 08:07:45 -0500

    Restore data from localstorage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,31 @@ import { useEffect, useState } from "react";
 const Home: NextPage = () => {
   const [age, setAge] = useState(0);
   const [search, setSearch] = useState("");
+
   useEffect(() => {
     (async () => {
-      const response = await fetch(
-        `http://localhost:3000/api/hello?name=${search}`
-      );
-      const data = await response.json();
-      setAge(data.age);
+      if (search) {
+        const response = await fetch(
+          `http://localhost:3000/api/hello?name=${search}`
+        );
+        const data = await response.json();
+        if (!age) {
+          setAge(data.age);
+        }
+        if (window.localStorage && search) {
+          localStorage.setItem(search, data.age);
+        }
+      }
     })();
+    console.log("times");
   }, [search]);
+
+  useEffect(() => {
+    const age = JSON.parse(localStorage.getItem("benja")!);
+    if (age) {
+      setAge(age);
+    }
+  }, []);
 
   return (
     <div>
